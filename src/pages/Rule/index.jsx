50673f86836042a4f24f56fa4f877232f9fa0215
Davranbek <davran9559@gmail.com>
2023-11-17 01:55:08 +0500

    add rule page

--- src/pages/Rule/index.jsx
+++ src/pages/Rule/index.jsx
@@ -3,8 +3,10 @@
 
 const index = () => {
     return (
-        <div>
-            rules
+        <div className="pt-[165px] pb-5">
+            <div className="container">
+                <h2 className="text-bold text-[20px]">Правило пользованиe</h2>
+            </div>
         </div>
     );
 };
